ab71935e1a6a17ad1a35b70f58223966979456f6
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2016-07-19 14:12:53 +0200

    `chain` now defines `:call` method on extended classes.

--- lib/dry/mutations/transactions/container.rb
+++ lib/dry/mutations/transactions/container.rb
@@ -4,7 +4,7 @@ module Dry
       Container = lambda do |whatever|
         return ->(*input) { input } unless whatever
         whatever.respond_to?(:call) ? whatever : Utils.Constant(whatever).tap do |p|
-          fail ArgumentError, "The argument must respond to :call, though #{k.inspect} passed." unless p.respond_to? :call
+          fail ArgumentError, "The argument must respond to :call, though #{whatever.inspect} passed." unless p.respond_to? :call
         end
       end
     end
--- lib/dry/mutations/transactions/dsl.rb
+++ lib/dry/mutations/transactions/dsl.rb
@@ -10,13 +10,21 @@ module Dry
       module DSL # :nodoc:
         include Dry::Monads::Either::Mixin
 
+        def self.extended base
+          fail Errors::TypeError.new("Extended class [#{base}] should not respond to :call, it is defined by this extension.") if base.respond_to?(:call)
+        end
+
         def chain **params
           return enum_for(:chain) unless block_given? # FIXME: Needed? Works? Remove?
 
           λ = Proc.new
 
-          ::Dry.Transaction(container: ::Dry::Mutations::Transactions::Container, step_adapters: StepAdapters) do
+          @transaction = ::Dry.Transaction(container: ::Dry::Mutations::Transactions::Container, step_adapters: StepAdapters) do
             instance_eval(&λ)
+          end.tap do |transaction|
+            singleton_class.send :define_method, :call do |input|
+              transaction.(input)
+            end
           end
         end
       end
